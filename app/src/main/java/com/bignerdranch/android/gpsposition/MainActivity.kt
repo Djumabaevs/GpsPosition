4198fe6aa3221a32fe31bcae4ac577804debc67b
Djumabaevs <djumabaevb@gmail.com>
2021-06-03 11:34:20 +0600

    add binding to inflate layout views

--- app/src/main/java/com/bignerdranch/android/gpsposition/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/gpsposition/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.bignerdranch.android.gpsposition
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.bignerdranch.android.gpsposition.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+
+    }
+
+    override fun onLocationChanged(location: Location) {
 
     }
 }
